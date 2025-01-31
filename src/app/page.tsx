a523b5cb76e21234e8f7b29736b1c15e0ddbbafc
Tim EKPB <timkpb2023@lampungprov.go.id>
2025-01-31 10:05:32 +0700

    fixing route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import HomePage from "./(no-auth)/home/page"
-import MainPage from "@/app/(main)/page"
+import MainPage from "@/app/(main)/main/page"
 import { getSession } from "../provider/api"
 const Page = async () => {
     const session = await getSession()
